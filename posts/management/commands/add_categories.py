93b3031178e2731feb62054a340e739aa0939120
zingjul <nandomshepherd@gmail.com>
2025-03-10 03:02:10 -0700

    after partial loss of data

--- posts/management/commands/add_categories.py
+++ posts/management/commands/add_categories.py
@@ -1,28 +1,14 @@
 from django.core.management.base import BaseCommand
-from posts.models import Category  # Import your Category model
+from posts.models import Category
 
 class Command(BaseCommand):
-    help = 'Adds predefined categories to the database'
+    help = 'Populates the Category model with default categories'
 
     def handle(self, *args, **options):
-        categories_to_add = [
-            "Technology",
-            "Travel",
-            "Food",
-            "Fashion",
-            "Sports",
-            "Science",
-            "Politics",
-            "Art",
-            "Music",
-            "Books",
-            # Add more categories here
-        ]
-
-        for category_name in categories_to_add:
-            # Check if the category already exists to avoid duplicates
+        categories = ['Goods', 'Services', 'Labor']
+        for category_name in categories:
             if not Category.objects.filter(name=category_name).exists():
                 Category.objects.create(name=category_name)
                 self.stdout.write(self.style.SUCCESS(f'Successfully created category: {category_name}'))
             else:
-                self.stdout.write(self.style.WARNING(f'Category already exists: {category_name}'))
+                self.stdout.write(self.style.WARNING(f'Category {category_name} already exists.'))
