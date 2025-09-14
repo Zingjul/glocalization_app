8fe6f8a902db83013f4a97e6d0ea51f25104bcc5
unknown <nandomshepherd@gmail.com>
2025-09-14 04:00:45 +0100

    before adding media_app app

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,10 +2,37 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+
+
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ["email", "username", "phone_number", "is_staff", "virtual_id",]
-    
-admin.site.register(CustomUser, CustomUserAdmin)
+
+    # Columns shown in user list
+    list_display = (
+        "username",
+        "email",
+        "phone_number",
+        "virtual_id",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = ("is_staff", "is_active")
+
+    # Organize fields on the user detail page
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("phone_number", "virtual_id")}),
+    )
+
+    # Fields shown when creating a new user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("phone_number",)}),  # exclude virtual_id since it auto-generates
+    )
+
+    # Make virtual_id readonly
+    readonly_fields = ("virtual_id",)
+
+    search_fields = ("username", "email", "phone_number")
+    ordering = ("username",)
